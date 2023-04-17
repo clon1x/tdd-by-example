2566d46a9ef77c76cc62e0f695ac25e6a377e5a5
LVL <luis.vinelerma@telefonica.com>
2023-04-17 15:00:27 +0200

    Failing test should_GiveCorrectAmounts_when_CalledMultipleTimes

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -1,6 +1,7 @@
 package guru.springframework;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -8,33 +9,40 @@ class MoneyTest {
 
 	@Test
 	void should_GiveCorrectAmount_when_MultipliedByFactor() {
-		
+
 		// given
 		Dollar money = new Dollar(5);
-		
+
 		// when
 		money.multiply(2);
-		
+
 		// then
 		assertEquals(10, money.getAmount());
-		
+
 	}
-	
+
 	@Test
 	void should_GiveCorrectAmounts_when_CalledMultipleTimes() {
-		
+
 		// given
 		final int DOLLARS_AMOUNT = 5;
-		final int MULTIPLICATION_FACTOR = 2;
-		final int EXPECTED = 10;
+		final int MULTIPLICATION_FACTOR_1 = 2;
+		final int EXPECTED_1 = 10;
+		final int MULTIPLICATION_FACTOR_2 = 3;
+		final int EXPECTED_2 = 15;
 		Dollar money = new Dollar(DOLLARS_AMOUNT);
-		
+
 		// when
-		money.multiply(MULTIPLICATION_FACTOR);
-		
+		money.multiply(MULTIPLICATION_FACTOR_1);
+		int actual1 = money.getAmount();
+		money.multiply(MULTIPLICATION_FACTOR_2);
+		int actual2 = money.getAmount();
+
 		// then
-		assertEquals(EXPECTED, money.getAmount());
-		
+		assertAll(
+				() -> assertEquals(EXPECTED_1, actual1), 
+				() -> assertEquals(EXPECTED_2, actual2));
+
 	}
 
 }
