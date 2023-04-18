c1a95a1cbd39554e4b3c9d6136641076ab08a7f1
LVL <luis.vinelerma@telefonica.com>
2023-04-18 16:09:25 +0200

    Rewrite equals method in Money class
    All tests pass

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -25,7 +25,7 @@ public class Money {
 	@Override
 	public boolean equals(Object obj) {
 		Money money = (Money) obj;
-		return amount == money.amount && getClass().equals(obj.getClass());
+		return amount == money.amount && currency == money.currency ;
 	}
 
 	public String getCurrency() {
