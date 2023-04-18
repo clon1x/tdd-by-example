23f79eedb706529263425453df479fffd756ed2a
LVL <luis.vinelerma@telefonica.com>
2023-04-18 16:50:36 +0200

    Remove duplication in DollarTest and FrancTest
    1. Write a test
    2. Make the code compile
    3. Run test to make sura that it fails
    3. Make the test pass (minimum code)
    4. Remove duplication                                    <-  (you are here)

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -3,18 +3,90 @@ package guru.springframework;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.Test;
 
 class MoneyTest {
 
+	private static final String CURRENCY_DOLLAR = "USD";
+	private static final String CURRENCY_FRANC = "CHF";
+
 	@Test
-	void should_ReturnFalse_When_ConcreteClassIsDifferent() {
+	void should_GiveCorrectAmount_when_MultipliedByFactor() {
+
+		// given
+		int amount = 5;
+		Money money = new Money(amount, CURRENCY_FRANC);
+		Money expected = new Money(amount * 2, CURRENCY_FRANC);
+
+		// when
+		Money actual = money.multiplyBy(2);
+
+		// then
+		assertEquals(expected, actual);
+
+	}
+
+	@Test
+	void should_GiveCorrectAmounts_when_CalledMultipleTimes() {
+
+		// given
+		int amount = 5;
+		int factor1 = 2;
+		int factor2 = 3;
+		Money expected1 = new Money(10, CURRENCY_FRANC);
+		Money expected2 = new Money(15, CURRENCY_FRANC);
+		Money money = new Money(amount, CURRENCY_FRANC);
+
+		// when
+		Money product1 = money.multiplyBy(factor1);
+		Money product2 = money.multiplyBy(factor2);
+
+		// then
+		assertAll(
+				() -> assertEquals(expected1, product1), 
+				() -> assertEquals(expected2, product2));
+
+	}
+	
+	@Test
+	void should_ConsiderEqual_When_ValueAndCurrencyAreEqual() {
+		
+		// given
+		int value = 1;
+		String currency = CURRENCY_FRANC;
+		Money expected = new Money(value, currency);
+		
+		// when
+		Money actual = new Money(value, currency);
+		
+		// then
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void should_ConsiderFrancsNotEqual_When_DifferentValues() {
+		
+		// given
+		int amount = 1;
+		String currency = CURRENCY_FRANC;
+		Money franc1 = new Money(amount, currency);
+		
+		// when
+		Money franc2 = new Money(amount + 1, currency);
+		
+		// then
+		assertNotEquals(franc1, franc2);
+	}	
+	
+	@Test
+	void should_ReturnFalse_When_CurrencyIsDifferent() {
 		
 		// given 
-		final int AMOUNT = 6;
-		Money money1 = new Money(AMOUNT, "USD");
-		Money money2 = new Money(AMOUNT, "CHF");
+		int amount = 6;
+		Money money1 = new Money(amount, CURRENCY_DOLLAR);
+		Money money2 = new Money(amount, CURRENCY_FRANC);
 		
 		// when
 		boolean actual = money1.equals(money2);
@@ -25,22 +97,23 @@ class MoneyTest {
 	}
 	
 	@Test
-	void should_ReturnCorrectCurrency_When_ClassIsDollar() {
+	void should_ReturnCorrectCurrency() {
 		
 		// given
-		Money dollar = new Money(1, "USD");
-		Money franc = new Money(1, "CHF");
-		String expectedDollar = "USD";
-		String expectedFranc = "CHF";
+		int amount = 1;
+		Money dollar = new Money(amount, CURRENCY_DOLLAR);
+		Money franc = new Money(amount, CURRENCY_FRANC);
+		String expectedDollarCurrency = CURRENCY_DOLLAR;
+		String expectedFrancCurrency = CURRENCY_FRANC;
 		
 		// when
-		String actualDollar = dollar.getCurrency();
-		String actualFranc = franc.getCurrency();
+		String actualDollarCurrency = dollar.getCurrency();
+		String actualFrancCurrency = franc.getCurrency();
 		
 		// then
 		assertAll(
-				() -> assertEquals(expectedDollar, actualDollar),
-				() -> assertEquals(expectedFranc, actualFranc));
+				() -> assertEquals(expectedDollarCurrency, actualDollarCurrency),
+				() -> assertEquals(expectedFrancCurrency, actualFrancCurrency));
 		
 		
 	}
