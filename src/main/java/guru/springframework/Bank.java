2669666d3143f958b3b5a4cefae23d94945f650d
LVL <luis.vinelerma@telefonica.com>
2023-04-20 15:38:27 +0200

    Add conversion rates to Bank
    Failing tests:
     - should_ReturnRightMoney_When_ReducingSumDifferentCurrency

--- src/main/java/guru/springframework/Bank.java
+++ src/main/java/guru/springframework/Bank.java
@@ -1,14 +1,41 @@
 package guru.springframework;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Bank {
+	
+	private class CurrencyPair {
+
+		private Currency fromCurrency;
+		private Currency toCurrency;
+
+		public CurrencyPair(Currency fromCurrency, Currency toCurrency) {
+			this.fromCurrency = fromCurrency;
+			this.toCurrency = toCurrency;
+		}
+
+		@Override
+		public int hashCode() {
+			return 1;
+		}
+		
+		
+	}
+	
+	private Map<CurrencyPair, Integer> conversionRates = new HashMap<>();
 
 	public Money reduce(Expression source, Currency toCurrency) {
 		return source.reduce(toCurrency);
 	}
 
-	public void addRate(Currency franc, Currency dollar, int i) {
-		// TODO Auto-generated method stub
-		
+	public void addRate(Currency fromCurrency, Currency toCurrency, Integer conversionFactor) {
+		CurrencyPair currencyPair = new CurrencyPair(fromCurrency, toCurrency);
+		conversionRates.put(currencyPair, conversionFactor);
+	}
+	
+	public Integer getRate(Currency fromCurrency, Currency toCurrency) {
+		return null;
 	}
 
 }
